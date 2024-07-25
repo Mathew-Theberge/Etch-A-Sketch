229644f5fa8bb4cb092f4bce545e40508f88bf06
Mathew Theberge <140298808+Mathew-Theberge@users.noreply.github.com>
2024-07-24 23:01:00 -0400

    finish function that sets up the grid of sqaures

--- script.js
+++ script.js
@@ -1,35 +1,29 @@
 const container = document.querySelector(".container")
-const containers = document.createElement("div")
 
 number = prompt("number")
-
-function createDiv() {
-  const div = document.createElement("div")
-  div.setAttribute("class", "sqaure")
-  containers.appendChild(div)
-}
+numberForSecondLoop = number
 
 function createContainers() {
   const containers = document.createElement("div")
   containers.setAttribute("class", "containers")
   container.appendChild(containers)
-
 }
 
-while (number > 0) {
-  createContainers()
-  number--
+function createSqauresInContainers() {
+  while (number > 0) {
+    createContainers()
+    number--
+  }
+  const containersList = document.querySelectorAll(".containers")
+  while (numberForSecondLoop > 0) {
+    containersList.forEach((item) => {
+      const div = document.createElement("div")
+      div.setAttribute("class", "sqaure")
+      item.appendChild(div)
+    })
+    numberForSecondLoop--
+  }
 }
 
- let containersList = document.querySelectorAll(".containers")
 
- console.log(containersList)
-let i = 12
-while (i > 0) {
-  containersList.forEach((item) => {
-    const div = document.createElement("div")
-    div.setAttribute("class", "sqaure")
-    item.appendChild(div)
-  })
-  i--
-}
+createSqauresInContainers()
